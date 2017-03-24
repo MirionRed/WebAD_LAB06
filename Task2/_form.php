58ebcc73aa813580715de37643a5da1c63335e79
InnOpyU <innopyu@gmail.com>
2017-03-24 22:34:36 +0800

    LAB06 Finished

--- Task2/_form.php
+++ Task2/_form.php
@@ -1,9 +1,53 @@
 <?php
+$method ="";
+$valid = 0;
+$salutation = $name = $email = $phone = "";
+$salutationERR=$nameERR=$emailERR=$phoneERR="";
   if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if(empty($_POST["salutation"])){
+      $salutationERR = "Please select salutation";
+    } else {
+      $salutation = test_input($_POST["salutation"]);
+    }
 
+    if(empty($_POST["name"])){
+      $nameERR = "Please enter name";
+    }else {
+      $name = test_input($_POST["name"]);
+    }
+
+    if(empty($_POST["email"])){
+      $emailERR = "Please enter email";
+    }else {
+      $email = test_input($_POST["email"]);
+      if(!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
+        $emailERR = "Invalid email";
+        //echo "Invalid email";
+        $valid--;
+      }
+    }
+
+    if(empty($_POST["phone"])){
+      $phoneERR = "Please enter phone number";
+    }else {
+      $phone = test_input($_POST["phone"]);
+      if (!preg_match("/^[0-9]*$/",$_POST["phone"])) {
+        $phoneERR = "Invalid phone number";
+        //echo "Invalid phone number";
+        $valid--;
+      }
+    }
+  }
+  else {
+    
   }
 
-  else {
-
+  function test_input($data){
+    global $valid;
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    $valid++;
+    return $data;
   }
 ?>
--- Task2/index.php
+++ Task2/index.php
@@ -3,5 +3,32 @@
 <head>
 </head>
 <body>
+  <?php include('_form.php'); ?>
+
+  <p><span class="error">* required field</span></p>
+  <form method="POST" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+    Salutation:
+    <input type="radio" name="salutation" value="Mr" <?php if(isset($salutation) && $salutation == "Mr") echo "checked"; ?>>Mr
+    <input type="radio" name="salutation" value="Ms" <?php if(isset($salutation) && $salutation == "Ms") echo "checked"; ?>>Ms
+    <input type="radio" name="salutation" value="Mrs" <?php if(isset($salutation) && $salutation == "Mrs") echo "checked"; ?>>Mrs
+    <input type="radio" name="salutation" value="Mdm" <?php if(isset($salutation) && $salutation == "Mdm") echo "checked"; ?>>Mdm
+    <span class="error">*<?php echo $salutationERR; ?></span><br>
+    Name:
+    <input type="text" name="name" value="<?php echo $name; ?>">
+    <span class="error">*<?php echo $nameERR; ?></span><br>
+    Email Address:
+    <input type="text" name="email" value="<?php echo $email; ?>">
+    <span class="error">*<?php echo $emailERR; ?></span><br>
+    Phone Number:
+    <input type="text" name="phone" value="<?php echo $phone;?>">
+    <span class="error">*<?php echo $phoneERR; ?></span><br>
+    Type of Enquiry:
+    <select>
+      <option value="General Enquiry">General Enquiry</option>
+      <option value="Complaints">Complaints</option>
+      <option value="Suggestions">Suggestions</option>
+    </select><br>
+    <input type="submit" value="Submit">
+  </form>
 </body>
 </html>
